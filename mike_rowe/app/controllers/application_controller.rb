cf465392074acc3803c9cadded93746894be7cf3
Ryan Zell <ryanzell36@gmail.com>
2016-01-26 14:13:48 -0600

    Fixed Login

--- mike_rowe/app/controllers/application_controller.rb
+++ mike_rowe/app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def current_user
-    @current_user ||= Teacher.find(session[:teacher_id]) if session[:teacher_id]
+  def current_user
+      @current_user ||= Teacher.find(session[:teacher_id]) if session[:teacher_id]
   end
   helper_method :current_user
 
