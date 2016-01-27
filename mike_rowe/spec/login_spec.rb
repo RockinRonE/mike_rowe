2fe8cedb86402409619c5880a2c8fab17daac177
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-01-27 12:07:24 -0600

    add login test

---
+++ mike_rowe/spec/login_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+require 'capybara/rspec'
+require 'capybara/dsl'
+
+feature "Signing in" do
+  background do
+    Teacher.create(:email => 'user@example.com', :password => 'caplin')
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit '/'
+      within("#login") do
+        fill_in 'Email', :with => 'user@example.com'
+        fill_in 'Password', :with => 'caplin'
+      end
+    click_button 'Log In'
+    expect(page).to have_content 'Log out'
+    # expect(page.current_path).to eq '/teachers/1'
+  end
+end
+
