1656cc4c2fae635d91afe7867b3b6131b172471d
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-01-27 14:04:47 -0600

    add h2 tages to teacher profile

--- mike_rowe/spec/login_spec.rb
+++ mike_rowe/spec/login_spec.rb
@@ -13,15 +13,13 @@ feature "Signing in" do
         fill_in 'Email', :with => 'user@example.com'
         fill_in 'Password', :with => 'caplin'
       end
+  # include 'login_user'
     click_button 'Log In'
     expect(page).to have_content 'Log out'
+
+    click_link 'Students'
+    expect(page).to have_content 'Check out all these Students!'
     # expect(page.current_path).to eq '/teachers/1'
   end
 
-  scenario "Be able to visit students page" do
-    visit '/'
-    click_link 'Students'
-    expect(page).to have_content 'Check out all these Students!'
-  end
 end
-
